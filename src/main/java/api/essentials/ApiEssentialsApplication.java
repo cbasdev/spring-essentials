985cf0a2c29a834f33d1e41524f1a8e88c92cb3c
Sebastian Velez <info.cbasdev@gmail.com>
2023-09-18 11:08:23 -0500

    :bricks: Creating base structure generator start-spring-io

---
+++ src/main/java/api/essentials/ApiEssentialsApplication.java
@@ -0,0 +1,13 @@
+package api.essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEssentialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/essentials/ApiEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package api.essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
